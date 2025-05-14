b84d89620da652c8c0f89a04a3506e5b26bbfe3f
Bijayash Chitrakar <chitrakar.bijayash@dishhome.com.np>
2025-05-14 06:40:12 +0545

    Update new-provision-web.sh

--- new-provision-web.sh
+++ new-provision-web.sh
@@ -22,3 +22,15 @@ apt install apache2 \
 sudo mkdir -p /srv/www
 sudo chown www-data: /srv/www
 curl https://wordpress.org/latest.tar.gz | sudo -u www-data tar zx -C /srv/www
+
+a2ensite wordpress
+a2enmod rewrite
+a2dissite 000-default
+service apache2 reload
+
+sudo -u www-data cp /srv/www/wordpress/wp-config-sample.php /srv/www/wordpress/wp-config.php
+
+sudo -u www-data sed -i 's/localhost/192.168.18.108/' /srv/www/wordpress/wp-config.php
+sudo -u www-data sed -i 's/database_name_here/wordpress/' /srv/www/wordpress/wp-config.php
+sudo -u www-data sed -i 's/username_here/wordpress/' /srv/www/wordpress/wp-config.php
+sudo -u www-data sed -i 's/password_here/admin123/' /srv/www/wordpress/wp-config.php
